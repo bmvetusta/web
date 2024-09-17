5f6867b725207f2f845046324de8f339d77ad666
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-09-17 20:37:43 +0200

    First version of the new website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/abonate': 'https://abonate.balonmanovetusta.com',
+  },
+  integrations: [react()],
+});
