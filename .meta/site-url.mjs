b44f5f8e9ae736c0a987ac1d2f1e6a98b028d4e7
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-09-20 01:49:16 +0200

    Formatting code

--- .meta/site-url.mjs
+++ .meta/site-url.mjs
@@ -1,6 +1,6 @@
 const isDev = import.meta.env.DEV;
-const hostnameWithPath = process.env.SITE_URL ?? process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'localhost:4321';
 
+const hostnameWithPath = process.env.SITE_URL ?? process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'localhost:4321';
 const scheme = isDev || hostnameWithPath.includes('localhost') ? 'http' : 'https';
 
 export const site = new URL(`${scheme}://${hostnameWithPath}`);
