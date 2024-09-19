59f4ce9683f3f9fc1c495c256be63d82c3e69f7a
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-09-20 01:45:04 +0200

    Fix right env var for domain while using vercel

--- .meta/site-url.mjs
+++ .meta/site-url.mjs
@@ -1,6 +1,5 @@
 const isDev = import.meta.env.DEV;
-const hostnameWithPath = process.env.SITE_URL ?? process.env.VERCEL_URL ?? 'localhost:4321';
-
+const hostnameWithPath = process.env.SITE_URL ?? process.env.VERCEL_PROJECT_PRODUCTION_URL ?? 'localhost:4321';
 
 const scheme = isDev || hostnameWithPath.includes('localhost') ? 'http' : 'https';
 
