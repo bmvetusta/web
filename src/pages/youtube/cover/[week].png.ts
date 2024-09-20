00ddbc5d67ef05a79bca7f11aee351e8779e3aea
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-09-20 16:24:31 +0200

    Export getWeekData

--- src/pages/youtube/cover/[week].png.ts
+++ src/pages/youtube/cover/[week].png.ts
@@ -48,7 +48,7 @@ async function getFontOptionsFromFontPaths(...fontPaths: string[]) {
   );
 }
 
-async function getWeekData(week?: number | string | null) {
+export async function getWeekData(week?: number | string | null) {
   if (!week) {
     return;
   }
