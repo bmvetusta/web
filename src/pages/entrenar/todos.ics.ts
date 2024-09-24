453883f00e8859c955f7b44cd50bcc96778af61a
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-09-24 19:40:57 +0200

    Some color

---
+++ src/pages/entrenar/todos.ics.ts
@@ -0,0 +1,22 @@
+import trainings from 'src/content/trainings/trainings.json' with { type: 'json' };
+import { generateTrainingICS } from 'src/services/generate-training-ics';
+
+export async function GET() {
+  const ics = trainings.map((placeToTrain) => generateTrainingICS(placeToTrain)).join('\n\n');
+
+  if (ics) {
+    const filename = `todos.ics`;
+
+    return new Response(ics, {
+      headers: {
+        'Content-Description': 'File Transfer',
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': `attachment; filename=${filename}`,
+      },
+    });
+  }
+
+  return new Response('', {
+    status: 204,
+  });
+}
