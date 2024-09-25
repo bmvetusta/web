2dd31ce460bc4e3e2e6127224400cbdbf4a0478f
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-09-25 19:52:29 +0200

    Optional env var for user agent when requesting rfebm api

--- src/services/rfebm/base-href.ts
+++ src/services/rfebm/base-href.ts
@@ -3,7 +3,9 @@ export function getRFEBMHeaders() {
     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
     'Connection': 'keep-alive',
     'Accept': 'application/json',
-    'User-Agent': `5&,?"->(1483>%1*!("%* 0''>8.38-"?",("2#,!$(1>:64?"?,#?*='")*2" =.70&"7*/5*IOS`,
+    'User-Agent':
+      process.env.RFEBM_USER_AGENT ??
+      `5&,?"->(1483>%1*!("%* 0''>8.38-"?",("2#,!$(1>:64?"?,#?*='")*2" =.70&"7*/5*IOS`,
     'Accept-Language': 'es-ES;q=1.0, en-ES;q=0.9',
   });
 
