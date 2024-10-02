3dc9697814921551ae9c9772a8a2e8b78051d3b4
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-10-02 16:16:16 +0200

    Fix issue retrieveing relative path

--- src/lib/get-relative-app-root-path.ts
+++ src/lib/get-relative-app-root-path.ts
@@ -3,5 +3,5 @@ import { join } from 'node:path';
 export function getRelativeAppRootPath(...paths: string[]): string {
   const isVercel = process.env.VERCEL === '1';
 
-  return isVercel ? join('.', ...paths) : join('..', '..', '..', '..', ...paths);
+  return isVercel ? join('.', ...paths) : join('..', '..', ...paths);
 }
