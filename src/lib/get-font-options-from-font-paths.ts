87a09d9e22c2dd542eea6282977eb6d851683806
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-10-03 20:13:25 +0200

    Use fetch

--- src/lib/get-font-options-from-font-paths.ts
+++ src/lib/get-font-options-from-font-paths.ts
@@ -1,15 +1,14 @@
-import { readFile } from 'node:fs/promises';
 import { getWeightNumberByName } from './get-weight-number-by-name';
 
-export async function getFontOptionsFromFontPaths(...fontPaths: string[]) {
+export async function getFontOptionsFromFontPaths(...fontPaths: URL[]) {
   return await Promise.all(
     fontPaths.map(async (fontPath) => {
       const fontFilePathLowerCase = fontPath.toString().toLocaleLowerCase('es-ES');
       const name = 'Alumni Sans';
       const weight = getWeightNumberByName(fontPath.toString());
       const style = fontFilePathLowerCase.includes('italic') ? 'italic' : 'normal';
-      const data = await readFile(fontPath); // Font as buffer
-      // const data = await fetch(fontPath).then((res) => res.arrayBuffer());
+      // const data = await readFile(fontPath); // Font as buffer
+      const data = await fetch(fontPath).then((res) => res.arrayBuffer());
 
       return {
         name,
