c8282801acdd7b1b15c6efd48ddeedc28d147439
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-10-03 20:36:40 +0200

    More changes to try to solve the image generation

--- src/lib/get-font-options-from-font-paths.ts
+++ src/lib/get-font-options-from-font-paths.ts
@@ -2,7 +2,8 @@ import { readFile } from 'node:fs/promises';
 import { getWeightNumberByName } from './get-weight-number-by-name';
 
 const isVercel = process.env.VERCEL === '1';
-const prePath = isVercel ? '../..' : '../../public';
+// const prePath = isVercel ? '../../../..' : '../../public';
+const prePath = isVercel ? '' : '/public';
 export async function getFontOptionsFromFontPaths(...fontPaths: string[]) {
   return await Promise.all(
     fontPaths.map(async (fontPath) => {
@@ -10,8 +11,8 @@ export async function getFontOptionsFromFontPaths(...fontPaths: string[]) {
       const name = 'Alumni Sans';
       const weight = getWeightNumberByName(fontPath.toString());
       const style = fontFilePathLowerCase.includes('italic') ? 'italic' : 'normal';
-      const url = new URL(prePath + fontPath, import.meta.url);
-      const data = await readFile(url.pathname); // Font as buffer
+      const url = process.cwd() + prePath + fontPath;
+      const data = await readFile(url); // Font as buffer
       // const data = await fetch(fontPath).then((res) => res.arrayBuffer());
       // const require = createRequire(import.meta.url);
 
