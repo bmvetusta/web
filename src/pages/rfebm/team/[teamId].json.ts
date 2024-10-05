8dadd6e585b7a30ca5348c8cb037dbcdc53d732a
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-10-05 13:28:48 +0200

    Changed to another endpoint

--- src/pages/rfebm/team/[teamId].json.ts
+++ src/pages/rfebm/team/[teamId].json.ts
@@ -1,11 +1,19 @@
-import type { APIContext } from 'astro';
-import { rfebmAPIGetTeam } from 'src/services/rfebm-api/get-team';
+import { rfebmAPIGetInitialData } from 'src/services/rfebm-api/get-initial-data';
 
 export const prerender = false;
 
-export async function GET({ params: { teamId } }: APIContext<{ teamId: string }>) {
-  const teamData = await rfebmAPIGetTeam(teamId);
-  return new Response(JSON.stringify(teamData), {
+export async function GET() {
+  // const teamData = await rfebmAPIGetTeam(teamId);
+  // return new Response(JSON.stringify(teamData), {
+  //   status: 200,
+  //   headers: {
+  //     'content-type': 'application/json',
+  //   },
+  // });
+
+  const data = await rfebmAPIGetInitialData();
+
+  return new Response(JSON.stringify(data), {
     status: 200,
     headers: {
       'content-type': 'application/json',
