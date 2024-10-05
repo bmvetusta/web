8bc0f6f7a0bb5c3335121f887a67e2c5f618f74c
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-10-05 21:39:14 +0200

    Using collections instead of api calls

--- src/content/calendar/index.ts
+++ src/content/calendar/index.ts
@@ -3,6 +3,7 @@ import { defineCollection } from 'astro:content';
 import { responseCalendarSchema } from 'src/schema/calendar/response';
 
 export const calendar = defineCollection({
+  type: 'content_layer',
   loader: file('src/content/team/data.json'),
   schema: responseCalendarSchema._output,
 });
