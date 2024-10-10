d21eae996fd08b42b42406545d9368e6c3c84760
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-10-10 23:17:59 +0200

    Removed feature flags, remove test page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 
-import type { FeatureFlags } from './pages/.well-known/vercel/flags';
-
 declare namespace App {
-  interface Locals {
-    flags: FeatureFlags;
-  }
+  interface Locals {}
 }
