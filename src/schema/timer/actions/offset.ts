4bbc97a15ac0234ddd1992775c160dabd8884ef2
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-11-08 21:11:00 +0100

    Allow to set offset as string hms

--- src/schema/timer/actions/offset.ts
+++ src/schema/timer/actions/offset.ts
@@ -2,6 +2,7 @@ import { TimerAction } from 'src/schema/timer/actions/type';
 import { timerNameSchema } from 'src/schema/timer/store';
 import { z } from 'zod';
 import { optionsTimerSchema } from '../options';
+import { timeInMsSchema } from '../time-in-ms';
 
 // TODO: Use of schema and not just types in addEventListener message & postMessage
 // TODO: Toggle
@@ -10,7 +11,7 @@ import { optionsTimerSchema } from '../options';
 export const OffsetActionTimer = z.object({
   type: z.union([z.literal(TimerAction.SET_OFFSET), z.literal(TimerAction.ADD_OFFSET)]),
   name: timerNameSchema,
-  payload: z.coerce.number(),
+  payload: timeInMsSchema,
   opts: optionsTimerSchema.optional(),
 });
 export type OffsetActionTimer = z.infer<typeof OffsetActionTimer>;
